d4954cb6ee21fb6baefb27e48b963a7bb4f762e1
hp <hp@DESKTOP-E9MUEHT>
2024-11-21 22:46:52 +0530

    first commit

---
+++ src/main/java/com/mycompany/minesweeper/App.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.minesweeper;
+
+/**
+ *
+ * @author hp
+ */
+public class App {
+    Minesweeper minesweeper = new Minesweeper();
+    
+}
---
+++ src/main/java/com/mycompany/minesweeper/Minesweeper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minesweeper;
+
+/**
+ *
+ * @author hp
+ */
+
+public class Minesweeper {
+       public static void main(String[] args) {
+       MinesweeperGui minesweeper = new MinesweeperGui();
+    }
+}
